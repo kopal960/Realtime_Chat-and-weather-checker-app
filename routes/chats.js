10742b463d3c05579c51331349a9ae7c29ae2bf4
kopal960 <kopalmsm@gmail.com>
2020-12-08 01:12:01 +0530

    chatname error fixed

--- routes/chats.js
+++ routes/chats.js
@@ -4,65 +4,70 @@ const Friends = require('../models/user')
 const Messages = require('../models/messages')
 
 router.get('/:chat_id', (req,res)=>{
-        if(!req.isAuthenticated()){
-            req.flash('error','Please Login');
-            return res.redirect('/login');
-        }
+    if(!req.isAuthenticated()){
+        req.flash('error','Please Login');
+        return res.redirect('/login');
+    }
+    else
+    {
+        data = {'id':req.user._id , 'chat_id' : req.params.chat_id};
+        Friends.findOne( {"_id" : data.id}, '_id username'   ,function(err , result){
+        if(err)
+        {res.status(500).send("internal server error")}
         else
         {
-            data = {'id':req.user._id , 'chat_id' : req.params.chat_id};
-            Friends.findOne( {"_id" : data.id}, '_id username'   ,function(err , result){
-            if(err)
-            {res.status(500).send("internal server error")}
+            Friends.find({} , '_id username online' , function(err, users){
+            if(err) 
+                res.status(500).send("Internal server error");
             else
-            {
-                Friends.find({} , '_id username online' , function(err, users){
-                if(err) 
-                    res.status(500).send("Internal server error");
+            {  
+                if(data.chat_id==0) 
+                    chat_id =0;
                 else
-                {
-                    var chatname;
-                    if(data.chat_id==0)
-                    {
-                        chatname = "Community";
-                        var chat_id =0
-                    }    
-                    else
-                    {
-                        Friends.findById(data.chat_id, function(err , friend){
-                            if(err) {
-                                res.status(404).send("Chat you are looking for is not found");
-                            } 
-                            else  chatname = friend.username;
-                        });
-                        var chat_id = (data.id > data.chat_id? data.chat_id+data.id: data.id+data.chat_id);
-                    }    
-                    Messages.find( {chat_id } ).lean().exec(
-                        function (err ,msgs) {
-                            if(err)
-                                { res.status(500).send("Internal Server error");}
-                            else
-                            {
-                                async function addinfo (){ 
-                                    msgs = await Promise.all( msgs.map(async (msg) =>  
-                                    {
-                                      await Friends.findById(msg.sender).then(friend =>{ msg["sendername"] =friend.username ; })
-                                     .catch(err => {res.status(500).send("Internal Server error")})
-                                      return msg
-                                    })
-                                )}
-                                addinfo().then(()=>{
-                                    res.render('chats.ejs', {users , receiver: result , msgs ,chatname , chat_id:data.chat_id} );
+                    var chat_id = (data.id > data.chat_id? data.chat_id+data.id: data.id+data.chat_id);
+                Messages.find( {chat_id } ).lean().exec(
+                    function (err ,msgs) {
+                        if(err)
+                            { res.status(500).send("Internal Server error");}
+                        else
+                        {
+                            var chatname;
+                            async function addinfo (){ 
+                                msgs = await Promise.all( msgs.map(async (msg) =>  
+                                {
+                                    await Friends.findById(msg.sender).then(friend =>{ msg["sendername"] =friend.username ; })
+                                    .catch(err => {res.status(500).send("Internal Server error")})
+                                    return msg
                                 })
+                            )}
+                            async function addname(){
+                                if(data.chat_id==0)
+                                {
+                                    chatname = "Community";
+                                }    
+                                else
+                                {
+                                    await Friends.findById(data.chat_id).then( friend=>{
+                                        if(err || !friend) 
+                                            res.status(404).send("Chat you are looking for is not found");
+                                        else
+                                            chatname = friend.username;
+                                    })
+                                } 
                             }
+                            addinfo().then(()=>{
+                                addname().then(()=>{
+                                    res.render('chats.ejs', {users , receiver: result , msgs , chatname , chat_id:data.chat_id} );
+                                })
+                            })
                         }
-                    ) 
-                }     
-            })
-        }
+                    }
+                ) 
+            }     
+        })
+    }
     })
-       }
-    }  
- )
+    }
+})
 
 module.exports = router;
